83d7f6562df1023461a9fbf5848104a771d2bc98
poeshead32 <152335585+poeshead32@users.noreply.github.com>
2023-11-29 10:58:32 +0200

    Update Phase_3.py

--- HuttonCourt/pages/Phase_3.py
+++ HuttonCourt/pages/Phase_3.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import plotly.express as px
+import numpy as np
 
 # Page title
-st.title("Access Energy - Phase 2 Proposal for Hutton Court")
+st.title("Phase 3: Diesel to Natural Gas Generator Conversion")
 
 # Cost Comparison
 st.subheader("Cost Comparison: Diesel vs. Natural Gas Generators")
@@ -26,25 +26,28 @@ EMISSIONS_FACTOR_GAS = 1.22    # kg CO2 per kWh for natural gas
 run_time = st.slider('Average Run Time (hours per day)', 0, 24, 8)
 emissions_diesel = run_time * EMISSIONS_FACTOR_DIESEL * 275
 emissions_gas = run_time * EMISSIONS_FACTOR_GAS * 275
-st.write(f"Daily emissions for diesel: {emissions_diesel} kg CO2")
-st.write(f"Daily emissions for natural gas: {emissions_gas} kg CO2")
+st.metric(label="Daily emissions for diesel (kg CO2)", value=f"{emissions_diesel:.2f}")
+st.metric(label="Daily emissions for natural gas (kg CO2)", value=f"{emissions_gas:.2f}")
 
 # Conversion Process Timeline
 st.subheader("Conversion Process Timeline")
-timeline_data = pd.DataFrame([
-    dict(Task="Planning", Start='2023-01-01', Finish='2023-02-01'),
-    dict(Task="Procurement", Start='2023-02-02', Finish='2023-03-01'),
-    dict(Task="Installation", Start='2023-03-02', Finish='2023-04-01'),
-    dict(Task="Testing", Start='2023-04-02', Finish='2023-05-01'),
-    dict(Task="Finalization", Start='2023-05-02', Finish='2023-06-01'),
-])
-fig = px.timeline(timeline_data, x_start="Start", x_end="Finish", y="Task", labels={'Task': 'Milestone'})
-fig.update_yaxes(autorange="reversed")  # otherwise tasks are listed from the bottom up
-st.plotly_chart(fig, use_container_width=True)
+timeline_data = pd.DataFrame({
+    'Milestone': ["Planning", "Procurement", "Installation", "Testing", "Finalization"],
+    'Start': pd.to_datetime(['2023-01-01', '2023-02-02', '2023-03-02', '2023-04-02', '2023-05-02']),
+    'End': pd.to_datetime(['2023-02-01', '2023-03-01', '2023-04-01', '2023-05-01', '2023-06-01']),
+})
+timeline_data = timeline_data.melt('Milestone', var_name='Date', value_name='Day')
+timeline_chart = alt.Chart(timeline_data).mark_bar().encode(
+    x='Day:T',
+    y='Milestone:N',
+    color='Date:N',
+    tooltip=['Milestone', 'Day', 'Date']
+).properties(width=600)
+st.altair_chart(timeline_chart, use_container_width=True)
 
 # Financial Modeling Tool
 st.subheader('Financial Modeling Tool')
-natural_gas_price = st.number_input('Natural Gas Price (R per kWh)', value=0.05)
+natural_gas_price = st.number_input('Natural Gas Price ($ per kWh)', value=0.05)
 expected_run_time = st.number_input('Expected Run Time (hours per year)', value=2000)
 financing_cost = st.number_input('Financing Cost (%)', value=5.0)
 investment = 100000  # Example investment cost for conversion
